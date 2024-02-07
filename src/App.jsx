df36ca96150988863ccdb01b5971533a5d54dff8
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-02-07 20:57:09 -0300

    Clase 2 - Proyecto de Vite

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Alumno from './components/Alumno.jsx'
+
+function App() {
+  console.log('Hola')
+  return (
+    <>
+      <h1>Mi segunda clase de FEIII</h1>
+      <ul>
+        <li>Alumno 1</li>
+        <li>Alumno 2</li>
+        <li>Alumno 3</li>
+        <li>Alumno 4</li>
+        <li>Alumno 5</li>
+      </ul>
+      <input type="text" />
+      <button>Hola</button>
+      <img src="" alt="" />
+      <Alumno/>
+      <Alumno/>
+      <Alumno/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Alumno.jsx
@@ -0,0 +1,10 @@
+const Alumno = () => {
+    return (
+        <div>
+            <h3>Alumno: Ariel</h3>
+            <h4>Nota: 9</h4>
+        </div>
+    )
+}
+
+export default Alumno
